0cd395d32b5f2d49f7a8c113e6c962d49e1b1dc9
Go Nishimoto <nishimoto.go@gmail.com>
2021-04-22 21:54:38 +0900

    タスク作成ページ作成

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# データベースを定義
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
