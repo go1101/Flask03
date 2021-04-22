ab8239a62c6d569c11f5cc4d225c6c91a29b6681
Go Nishimoto <nishimoto.go@gmail.com>
2021-04-22 22:04:39 +0900

    no message

--- app.py
+++ app.py
@@ -3,20 +3,26 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-# データベースを定義
+# データベースをURI設定
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
 db = SQLAlchemy(app)
 
-
+# データベースの項目定義
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(30), nullable=False)
     detail = db.Column(db.String(100))
     due = db.Column(db.DateTime, nullable=False)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
