fec8a80949dd51e19a49c8bda3dee308746c6ceb
go1101 <nishimoto.go@gmail.com>
2021-05-05 23:52:23 +0900

    期限切れのタスクに警告メッセージを表示

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from datetime import datetime, date
 
 
 # ブランチを作成
@@ -23,7 +23,7 @@ class Post(db.Model):
 def index():
     if request.method == 'GET':
         posts = Post.query.order_by(Post.due).all()
-        return render_template('index.html', posts=posts)
+        return render_template('index.html', posts=posts, today=date.today())
     else:
         title = request.form.get('title')
         detail = request.form.get('detail')
